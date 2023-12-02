875567ef708f79d6735676c101c10928f9683fe1
krushnabiradar <krushnabiradar1995@.com>
2023-12-02 21:10:40 +0530

    removed unnecessary dependencies

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
